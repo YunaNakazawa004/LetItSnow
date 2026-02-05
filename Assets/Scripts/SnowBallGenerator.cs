a94be61acad85d01c7495722a8c6f0f151f34209
Yuna Nakazawa <251322@st.yoshida-g.ac.jp>
2026-02-05 10:16:05 +0900

    プレイヤーとカメラの調整

--- Assets/Scripts/SnowBallGenerator.cs
+++ Assets/Scripts/SnowBallGenerator.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SnowBallGenerator : MonoBehaviour
 {
     public GameObject snowballPrefab;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +18,12 @@ public class SnowBallGenerator : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject snowball = Instantiate(snowballPrefab);
+            Vector3 shootPos = player.transform.position + Vector3.up * 2.0f; // ì™ÇÃçÇÇ≥
+            shootPos += player.transform.forward * 1.5f; // ëÃÇÃëO
 
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            GameObject snowball = Instantiate(snowballPrefab, shootPos, Quaternion.identity);
+
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
             Vector3 worldDir = ray.direction;
             snowball.GetComponent<SnowBallManager>().Shoot(worldDir.normalized * 2000);
         }

2db41d6351b167baf523bade1a323c5160b678bd
soki inamura <251305@st.yoshida-g.ac.jp>
2026-02-05 12:30:03 +0900

    スコアの追加。表示まで

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text playerScoreText;    // プレイヤーのスコアテキスト
+    public Text enemyScoreText;     // 敵のスコアテキスト
+
+    private int nScorePlayer;       // プレイヤーのスコア
+    private int nScoreEnemy;        // 敵のスコア
+
+    //=====================================================
+    // 開始処理
+    //=====================================================
     void Start()
     {
-        
+        nScorePlayer = 0;   // プレイヤーのスコアを初期化
+        nScoreEnemy = 0;    // 敵のスコアを初期化
     }
 
-    // Update is called once per frame
+    //=====================================================
+    // 更新処理
+    //=====================================================
     void Update()
     {
-        
+        // 表示テキストの設定
+        playerScoreText.text = "PLAYER\n" + nScorePlayer;
+        enemyScoreText.text = "ENEMY\n" + nScoreEnemy;
     }
+
+    //========================================
+    // スコアの更新処理処理
+    //========================================
+    void UpScore(string tag)
+    {
+        if(Equals(tag, "player"))
+        {// 当たったのがプレイヤー
+
+            nScoreEnemy++;      // 敵のスコアを加算
+        }
+        else if(Equals(tag, "enemy"))
+        {// 当たったのが敵
+
+            nScorePlayer++;      // プレイヤーのスコアを加算
+        }
+    }
+
 }
+

f30a64dd7282e6c7cdef80e62b6eb70313f007e2
UemuraMutsuki <251308@st.yoshida-g.ac.jp>
2026-02-12 11:40:46 +0900

    hennkou

--- Assets/Scripts/SnowBallManager.cs
+++ Assets/Scripts/SnowBallManager.cs
@@ -41,6 +41,7 @@ public class SnowBallManager : MonoBehaviour
         else if(!other.gameObject.CompareTag("Player") && !other.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
+            Score.UpScore("Player", 1);
         }
     }
 
